648b47ba5d718ff49ff962e472623e3302e30c49
Filipe Silva <filipe.silva@pintobrasil.com>
2025-12-09 14:13:32 +0000

    creating Position class

--- chess-console/Program.cs
+++ chess-console/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using board;
+
+namespace chess_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Position pos = new Position(3, 4);
+
+            Console.WriteLine("Position: " + pos);
+
+            Console.ReadLine();
+        }
+    }
+}
