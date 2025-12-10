dd2542f360c59a363e85cb21b1000235bba557df
Filipe Silva <filipe.silva@pintobrasil.com>
2025-12-10 13:59:50 +0000

    add special moves

--- chess-console/chess/king.cs
+++ chess-console/chess/king.cs
@@ -1,12 +1,14 @@
 ﻿using board;
+using chess;
 
 namespace chess_console.chess
 {
     class King : Piece
     {
-        public King(Board board, Color color) : base(board, color)
+        private ChessGame chessGame;
+        public King(Board board, Color color, ChessGame chessGame) : base(board, color)
         {
-
+            this.chessGame = chessGame;
         }
 
         public override string ToString()
@@ -77,7 +79,41 @@ namespace chess_console.chess
                 movements[pos.row, pos.column] = true;
             }
 
+            //# special move castling
+            if(moveCount == 0 && !chessGame.check)
+            {
+                // king side rook
+                Position posT1 = new Position(Position.row, Position.column + 3);
+                if (testRookCastling(posT1))
+                {
+                    Position p1 = new Position(Position.row, Position.column + 1);
+                    Position p2 = new Position(Position.row, Position.column + 2);
+                    if (board.piece(p1) == null && board.piece(p2) == null)
+                    {
+                        movements[Position.row, Position.column + 2] = true;
+                    }
+                }
+                // queen side rook
+                Position posT2 = new Position(Position.row, Position.column - 4);
+                if (testRookCastling(posT2))
+                {
+                    Position p1 = new Position(Position.row, Position.column - 1);
+                    Position p2 = new Position(Position.row, Position.column - 2);
+                    Position p3 = new Position(Position.row, Position.column - 3);
+                    if (board.piece(p1) == null && board.piece(p2) == null && board.piece(p3) == null)
+                    {
+                        movements[Position.row, Position.column - 2] = true;
+                    }
+                }
+            }
+
             return movements;
+        }
+
+        private bool testRookCastling(Position position)
+        {
+            Piece p = board.piece(position);
+            return p != null && p is Tower && p.color == color && p.moveCount == 0;
         }
     }
 }
